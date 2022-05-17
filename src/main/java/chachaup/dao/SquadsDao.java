0d674b90397c23af687b829c9b2ce7d380300a60
chacha <chachaerickjo@gmail.com>
2022-05-18 00:00:40 +0300

    ft - implements methods provided by the squads interface

---
+++ src/main/java/chachaup/dao/SquadsDao.java
@@ -0,0 +1,43 @@
+package chachaup.dao;
+
+import chachaup.domain.Squads;
+import chachaup.interfaces.iSquads;
+
+import java.util.List;
+
+public class SquadsDao implements iSquads {
+    @Override
+    public void addSquad(Squads squad) {
+
+    }
+
+    @Override
+    public void addHero(int heroId) {
+
+    }
+
+    @Override
+    public Squads findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Squads> findByCause(String cause) {
+        return null;
+    }
+
+    @Override
+    public List<Squads> getAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
