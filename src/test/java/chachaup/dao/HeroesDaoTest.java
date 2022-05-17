05d53c44777dcaa626cd7a7fe7c8f4d44e855916
chacha <chachaerickjo@gmail.com>
2022-05-17 23:56:00 +0300

    test - add test file for hero dao

---
+++ src/test/java/chachaup/dao/HeroesDaoTest.java
@@ -0,0 +1,16 @@
+package chachaup.dao;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class HeroesDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ src/test/java/chachaup/dao/SquadsDaoTest.java
@@ -0,0 +1,17 @@
+package chachaup.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquadsDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
