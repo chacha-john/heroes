e6e4042b71adf2eaef8b75f1996aa1a2366ba79a
chacha <chachaerickjo@gmail.com>
2022-05-19 14:38:07 +0300

    ft - add port wiring for deployment

--- src/main/java/chachaup/Main.java
+++ src/main/java/chachaup/Main.java
@@ -18,7 +18,15 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class Main extends RouterUtil {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/chachaup", "riko", "nzfu5321");
         SquadsDao squadsDao = new SquadsDao(sql2o);
         HeroesDao heroesDao = new HeroesDao(sql2o);
